4a04316715b29e228749049fd6166ee25659dfb5
Finn Gärtner <finngaertner@protonmail.com>
2024-02-29 00:53:12 +0100

    feat: Base layouts + Search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://wiki.vocadb.net/",
+  build: {
+    format: "file",
+  },
+  integrations: [react(), tailwind(), sitemap(), mdx(), pagefind()],
+});
---
+++ src/site_config.ts
@@ -0,0 +1,17 @@
+// Metadata, SEO, and Social
+export const SITE_TITLE = "VocaDB Wiki";
+export const SITE_DESCRIPTION = "Guideline and rules documentation for VocaDB";
+export const SITE_URL = "https://wiki.vocadb.net/";
+export const SITE_DEFAULT_OG_IMAGE = "/images/vocadb.jpg";
+
+// Docs Sidebar
+// Define the left sidebar items here.
+// The path should match the folder name in src/content/docs/
+export const SIDEBAR_ITEMS = {
+  "Get Started": [
+    "/docs/get-started/introduction/",
+    "/docs/get-started/installation/",
+  ],
+  Tutorials: ["/docs/tutorials/build-x/"],
+  Guides: ["/docs/guides/migrate-from-z/"],
+};

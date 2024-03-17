53f03d8a5f7f6f27a121fb35d3f46df182b623d8
Finn Gärtner <finngaertner@protonmail.com>
2024-03-17 16:15:54 +0100

    feat: Multi level navigation in mobile nav

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,42 +1,92 @@
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet";
+  ChevronDownIcon,
+  ChevronUpIcon,
+  HamburgerMenuIcon,
+} from "@radix-ui/react-icons";
+import { groupedNavbarParents, navbarParents, posts } from "./navigationConfig";
+import { ScrollArea } from "./ui/scroll-area";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { useState } from "react";
 
 export default function MobileNavbar() {
+  const [expanded, setExpanded] = useState<string | undefined>();
+
   return (
     <Sheet>
       <SheetTrigger
         aria-label="Open navigation menu"
         className="mr-6 md:hidden"
       >
-        <svg
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
-            fill="currentColor"
-            fillRule="evenodd"
-            clipRule="evenodd"
-          ></path>
-        </svg>
+        <HamburgerMenuIcon />
       </SheetTrigger>
       <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+        <ScrollArea className="pl-6 h-full">
+          {navbarParents
+            .map((i) => {
+              return {
+                navbarItem: i,
+                parentEntry: posts.find(
+                  (p) => p.params.slug === i.title.toLowerCase()
+                )!,
+              };
+            })
+            .map(({ navbarItem, parentEntry }) => (
+              <div
+                key={navbarItem.title}
+                className="flex flex-col space-y-3 pt-6"
+              >
+                <h4 className="font-medium">{navbarItem.title}</h4>
+                {navbarItem.subcategories !== undefined &&
+                  navbarItem.subcategories.length > 0 && (
+                    <>
+                      {navbarItem.subcategories.map((c) => (
+                        <div key={c}>
+                          <div
+                            className="collapsibleNav group flex flex-row items-center gap-1 peer text-muted-foreground hover:underline cursor-pointer"
+                            data-state={c === expanded ? "open" : "closed"}
+                            onClick={() =>
+                              c === expanded
+                                ? setExpanded(undefined)
+                                : setExpanded(c)
+                            }
+                          >
+                            {c}
+                            <ChevronDownIcon className="group-data-[state=closed]:block hidden" />
+                            <ChevronUpIcon className="group-data-[state=closed]:hidden" />
+                          </div>
+                          <div className="peer-data-[state=open]:grid hidden text-muted-foreground py-1 ml-4">
+                            {groupedNavbarParents[c]?.map((post) => (
+                              <a
+                                className="hover:underline pt-2"
+                                href={[
+                                  "/docs",
+                                  c.toLowerCase(),
+                                  post.params.slug,
+                                ].join("/")}
+                              >
+                                {post.props.entry.data.title}
+                              </a>
+                            ))}
+                          </div>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                {groupedNavbarParents[navbarItem.title]?.map((post) => (
+                  <a
+                    href={[
+                      "/docs",
+                      parentEntry.params.slug,
+                      post.params.slug,
+                    ].join("/")}
+                    className="text-muted-foreground"
+                  >
+                    {post.props.entry.data.title}
+                  </a>
+                ))}
+              </div>
+            ))}
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );

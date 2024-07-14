cbfa796e525a42f515429d837c6e79cba5c98950
Finn Gärtner <finngaertner@protonmail.com>
2024-07-14 20:15:46 +0200

    chores: Move development wiki to main wiki

--- src/components/navigationConfig.ts
+++ src/components/navigationConfig.ts
@@ -2,8 +2,19 @@ import { groupBy } from "@/lib/utils";
 import { getDocPaths } from "@/utils/get_paths";
 
 export const navbarParents = [
-  { title: "Guidelines", subcategories: ["Pinned", "Songs", "Albums", "Artists", "Secondary-entry-types", "Other-guidelines"] },
+  {
+    title: "Guidelines",
+    subcategories: [
+      "Pinned",
+      "Songs",
+      "Albums",
+      "Artists",
+      "Secondary-entry-types",
+      "Other-guidelines",
+    ],
+  },
   { title: "Documentation" },
+  { title: "Development" },
   { title: "Other" },
   { title: "UtaiteDB" },
   { title: "TouhouDB" },

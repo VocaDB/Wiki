e8a98b85d4575771b5a886516f5e61785f9d31d4
Hans5958 <hans5958@outlook.com>
2025-12-09 17:46:03 +0700

    Use shared Box component for other boxes

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-const defaultBoxClass = "relative w-full border rounded shadow my-5 group-[.reg]:my-0 group-[.reg]:shadow-none group-[.reg]:rounded-none group-[.reg]:border-0 group-[.reg]:border-b group-[.reg]:last:border-0";
+export const genericBoxClass = "w-full border rounded shadow relative bg-card text-card-foreground"
+
+const defaultBoxClass = cn(genericBoxClass, "my-5 group-[.reg]:my-0 group-[.reg]:shadow-none group-[.reg]:rounded-none group-[.reg]:border-0 group-[.reg]:border-b group-[.reg]:last:border-0");
 
 const Box = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn(defaultBoxClass, className)} {...props} />
 ))
 Box.displayName = "Box"
 
-const defaultBoxGroupClass = "group reg rounded border shadow my-5";
+const defaultBoxGroupClass = cn(genericBoxClass, "group reg my-5");
 
 const BoxGroup = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn(defaultBoxGroupClass, className)} {...props} />

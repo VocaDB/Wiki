8c83238b5e29749b9c4c65fb5bc5486bbf179f04
Hans5958 <hans5958@outlook.com>
2025-12-11 13:07:11 +0700

    Move generic box to outside ui/
    Requested on https://github.com/VocaDB/Wiki/pull/66#discussion_r2608191818
    Finn confused as why a non-shadcn/ui is on here, sorry mate

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 import { CheckIcon, Cross2Icon, ExclamationTriangleIcon, InfoCircledIcon } from "@radix-ui/react-icons";
-import { Box } from "@/components/ui/box";
+import { Box } from "@/components/Box";
 
 const alertVariants = cva(
   "px-4",
